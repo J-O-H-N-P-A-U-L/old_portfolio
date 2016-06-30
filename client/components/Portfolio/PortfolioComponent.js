7416c6adf114eb96a8e55f14f85282b56ee5e0b1
JPHUNTER <john.paul.hunter@hotmail.com>
2016-06-30 21:10:46 +1000

    make it better

--- client/components/Portfolio/PortfolioComponent.js
+++ client/components/Portfolio/PortfolioComponent.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 import React from 'react';
-import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
+import { Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 import Page from '../Page/PageComponent';
 import styles from './Portfolio.scss';
 
@@ -16,15 +16,18 @@ export default class Portfolio extends React.Component {
           {this.props.viewer.works.edges.map(edge => {
             const imageUrl = require(`../../assets/${edge.node.name.toLowerCase()}.gif`);
             return (
-              <Cell col={4} key={edge.node.id}>
-                <Card className={styles.card}>
-                  <CardTitle expand className={styles.image} style={{ backgroundImage: `url(${imageUrl})` }} />
-                  <CardActions className={styles.name}>
-                    <Button colored href={edge.node.url}>{edge.node.name}</Button>
-                  </CardActions>
-                  <CardText className={styles.description}>
-                    {edge.node.description}
+              <Cell col={12} key={edge.node.id}>
+                <Card shadow={0} className={styles.card}>
+                  <CardTitle className={styles.image} style={{ background: `url(${imageUrl})` }}>{edge.node.name}</CardTitle>
+                  <CardText>
+                  {edge.node.description}
                   </CardText>
+                  <CardActions border>
+                    <Button colored href={edge.node.url} target='_blank'>Visit Site</Button>
+                  </CardActions>
+                  <CardMenu style={{ color: '#fff' }}>
+                    <IconButton name='share' />
+                  </CardMenu>
                 </Card>
               </Cell>
             );
